456ae4c6813ce563436b88f8d9705a8ce5aee927
carlainha <carolradichi23@gmeil.com>
2025-06-11 08:53:36 -0300

    Aula dia 11/06

--- src/main/java/com/carol/food/domain/model/Restaurante.java
+++ src/main/java/com/carol/food/domain/model/Restaurante.java
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -23,6 +26,14 @@ public class Restaurante {
 
     @Column(name = "taxa_frete")
     private BigDecimal taxaFrete;
+
+    @UpdateTimestamp
+    @Column(name = "data_atualizacao", columnDefinition = "datetime")
+    private LocalDateTime dataAtualizacao;
+
+    @CreationTimestamp
+    @Column(name = "data_cadastro", columnDefinition = "datetime")
+    private LocalDateTime dataCadastro;
 
     @ManyToOne
     private Cozinha cozinha;
